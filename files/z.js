f739a12f05971a2e6b173aadd56b1b010fbc67ba
Deploy Server <deployserver@deployserver.local>
2016-08-11 14:15:05 +0800

    fix

---
+++ files/z.js
@@ -0,0 +1,32 @@
+var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true
+});
+
+function unload() {
+	$('.overlay').one('click', function(e){
+	    $(this).fadeOut(function(){
+	    	swiper.slideNext()
+	    	$(this).css('display','none')
+	    })
+	})
+}
+
+$('.choice.c_1').on('click', function(){
+    $('.overlay.c_1').fadeIn(function(){
+    	unload()
+    })
+})
+
+$('.choice.c_2').on('click', function(){
+    $('.overlay.c_2').fadeIn(function(){
+    	unload() 	
+    })
+})
+
+$('.choice.c_3').on('click', function(){
+    $('.overlay.c_2').fadeIn(function(){
+    	unload()
+    })
+})
+
