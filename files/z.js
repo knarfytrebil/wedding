302dece07edc71ce390d147dfab72b2a73cb1584
Frank <knarfytrebil@gmail.com>
2016-08-14 21:32:16 +0800

    fix

--- files/z.js
+++ files/z.js
@@ -3,33 +3,6 @@ var swiper = new Swiper('.swiper-container', {
     paginationClickable: true,
 });
 
-function unload() {
-	$('.overlay').one('click', function(e){
-	    $(this).fadeOut(function(){
-	    	swiper.slideNext()
-	    	$(this).css('display','none')
-	    })
-	})
-}
-
-$('.choice.c_1').on('click', function(){
-    $('.overlay.c_1').fadeIn(function(){
-    	unload()
-    })
-})
-
-$('.choice.c_2').on('click', function(){
-    $('.overlay.c_2').fadeIn(function(){
-    	unload() 	
-    })
-})
-
-$('.choice.c_3').on('click', function(){
-    $('.overlay.c_2').fadeIn(function(){
-    	unload()
-    })
-})
-
 
 $('#hint').on('click', function(){
     $('.overlay.c_3').fadeIn(function(){
