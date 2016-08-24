ed9a2241c440edbaf29c595fc15a6b59d1793cb0
Deploy Server <deployserver@deployserver.local>
2016-08-24 13:31:16 +0800

    fix

--- files/z.js
+++ files/z.js
@@ -13,11 +13,3 @@ $('#hint').on('click', function(){
 		})
     })
 })
-
-$(function{
-	if(navigator.userAgent.match(/MicroMessenger/i)){
-		var weixinShareLogo = 'http://0924.refle.xin/files/couple.jpg';
-		$('body').prepend('<div style=" overflow:hidden; width:0px; height:0; margin:0 auto; position:absolute; top:-800px;"><img src="'+ weixinShareLogo +'"></div>') 
-	};
-});
-
